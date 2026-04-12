d6ea4e16f2dcc9ef03f4f2d425326008b879a468
Masvel <masvel.labelplatform@gmail.com>
2026-04-12 16:57:43 +0300

    update cv link

--- src/shared/config/contacts.ts
+++ src/shared/config/contacts.ts
@@ -3,5 +3,5 @@ export const contacts = {
   telegram: "https://t.me/msvl_tg",
   github: "https://github.com/masvel-dev",
   linkedin: "https://www.linkedin.com/in/alexander-k-4b057b400/",
-  cv: "https://drive.google.com/file/d/1pjHGt5TzQ58ONre5F9wOnxFT0yn2L6IN/view"
+  cv: "https://drive.google.com/file/d/15XBI5DWU4-MbU6RkTuKFb2DsGfMN1RTM/view"
 };
